f74a2a396402b9aaef0da3c1f790e33d476d7d6c
FX918 <72587476+FX918@users.noreply.github.com>
2021-02-07 11:39:36 +0800

    0

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+function 左轉 () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+}
+function 右轉 () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+}
+function 後退 () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+}
+function 直線 () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+}
 basic.forever(function () {
-	
+    右轉()
+    basic.pause(2000)
+    後退()
+    basic.pause(2000)
+    直線()
 })
